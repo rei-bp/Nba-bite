d5e6675c0823be19713fa35908c7aa92e53b0fff
rei-bp <reinard.bp@gmail.com>
2021-06-10 22:16:22 -0700

    solve delete route - finish first model routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const ejs = require('ejs')
 const layouts = require('express-ejs-layouts')
 const axios = require('axios')
+const methodOverride = require('method-override')
 
 //config
 const app = express()
@@ -13,11 +14,13 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(layouts)
 app.use(express.static(__dirname + '/public/'))
+app.use(methodOverride('_method'))
 
 //controllers
 app.use('/teams', require('./routes/teams'))
 app.use('/players', require('./routes/players'))
-app.use('/stats', require('./routes/stats'))
+app.use('/favorites', require('./routes/favorites'))
+// app.use('/fantasy', require('./routes/fantasy')) 
 
 
 
@@ -26,13 +29,7 @@ app.use('/stats', require('./routes/stats'))
 app.get('/', (req, res) => {
 res.render('index.ejs')
 })
-// app.get('/', (req, res) => {
-// //     axios.get(`https://www.balldontlie.io/api/v1/teams`)
-// //     .then (apiRes => {
-// //         console.log(apiRes.data)
-// //         res.send(apiRes.data)
-// //     })
-// })
+
 
 
 
---
+++ routes/favorites.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const router = express.Router()
+const axios = require('axios')
+const db = require('../models')
+
+
+
+router.get('/', (req, res) => {
+    db.players.findAll({    
+        attributes: ['firstName', 'lastName', 'playerId', 'team']
+    })
+    .then (players => {
+        res.render('favorites/index.ejs', {players: players})
+    })
+})
+
+router.post('/new/:id', (req, res) => {
+    axios.get(`https://www.balldontlie.io/api/v1/players/${req.params.id}`)
+    .then (apiRes => {
+    db.players.create({
+        firstName: apiRes.data.first_name,
+        lastName: apiRes.data.last_name,
+        playerId: apiRes.data.id,
+        team: apiRes.data.team.full_name
+        }).then (player => {
+            console.log('success')
+            res.redirect('/favorites')
+        })
+        .catch ((err) => {
+            console.log(err)
+        })
+    })
+})
+
+
+router.get('/:id', (req, res) => {
+
+    const axOne = axios.get(`https://www.balldontlie.io/api/v1/players/${req.params.id}`)
+    const axTwo = axios.get(`https://www.balldontlie.io/api/v1/season_averages?player_ids[]=${req.params.id}`)
+    axios.all([axOne, axTwo])
+    .then (axios.spread((...responses) => {
+        const resOne = responses[0]
+        const resTwo = responses[1]
+        res.render('players/detail.ejs', {resOne: resOne.data, resTwo: resTwo.data.data[0]})
+    }))
+    .catch ((err) => {
+        console.log(err)
+    })
+})
+
+
+router.delete('/delete/:id', (req, res) => {
+    db.players.destroy({
+    where: {
+        playerId: req.params.id
+    }
+    })
+    .then (player => {
+        res.redirect('/favorites')
+    })
+})
+
+
+module.exports = router
